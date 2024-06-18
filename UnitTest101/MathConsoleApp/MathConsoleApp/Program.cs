d24856d716f77006a1ba07d1ee635324aa3bdaad
6551 <roger.chao@tradevan.com.tw>
2024-06-18 09:20:57 +0800

    a simple unit test project

---
+++ UnitTest101/MathConsoleApp/MathConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MathLibrary;
+
+namespace MathConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperations = new MathOperations();
+
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = mathOperations.Add(num1, num2);
+
+            Console.WriteLine($"The result of adding {num1} and {num2} is: {result}");
+        }
+    }
+}
---
+++ UnitTest101/MathConsoleApp/MathLibrary.Tests/MathOperationsTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using MathLibrary;
+
+namespace MathLibrary.Tests
+{
+    [TestFixture]
+    public class MathOperationsTests
+    {
+        private MathOperations _mathOperations;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mathOperations = new MathOperations();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfArguments()
+        {
+            var result = _mathOperations.Add(2, 3);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        [TestCase(1, 2, 3)]
+        [TestCase(-1, -2, -3)]
+        [TestCase(-1, 1, 0)]
+        [TestCase(100, 200, 300)]
+        [TestCase(0, 0, 0)]
+        public void Add_WhenCalled_ReturnsSumOfArguments2(int a, int b, int expected)
+        {
+            var result = _mathOperations.Add(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
